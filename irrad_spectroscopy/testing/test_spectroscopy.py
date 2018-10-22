5d28a55bba35f44dde3424646c4a6c80c436a73b
leloup314 <s6plwolf@uni-bonn.de>
2018-10-22 17:24:31 +0200

    ENH: test to find peaks from library instead of giving expected peaks; MAINT: adapt tests to restructuring

--- irrad_spectroscopy/testing/test_spectroscopy.py
+++ irrad_spectroscopy/testing/test_spectroscopy.py
@@ -3,15 +3,14 @@ import logging
 import yaml
 import numpy as np
 import unittest
-import irrad_spectroscopy
-
-from irrad_spectroscopy.spectroscopy import fit_spectrum, fit_background, do_energy_calibration, do_efficiency_calibration, get_activity
-from irrad_spectroscopy.utils.utils import get_measurement_time, isotopes_to_dict, calc_activity, source_to_dict
+import irrad_spectroscopy as isp
+import irrad_spectroscopy.spectroscopy as sp
+from irrad_spectroscopy.spec_utils import get_measurement_time, isotopes_to_dict, source_to_dict
 
 # Get the absolute path of the irrad_spectroscopy installation
-package_path = os.path.dirname(irrad_spectroscopy.__file__)
+package_path = isp.package_path
 
-testing_path = os.path.dirname(os.path.abspath(__file__))
+testing_path = isp.testing_path
 
 test_data_path = os.path.join(testing_path, 'test_data')
 
@@ -36,10 +35,6 @@ class TestSpectroscopy(unittest.TestCase):
         
         with open(os.path.join(test_data_path, '152Eu_peaks.yaml'), 'r') as energy_peaks:
             cls.energy_calib_peaks = yaml.safe_load(energy_peaks)
-            
-        # library yaml
-        with open(os.path.join(package_path, 'isotope_lib.yaml'), 'r') as iso_lib:
-            cls.isotope_lib = yaml.safe_load(iso_lib)
         
         # benchmark source specs of Na22 and Ba133
         with open(os.path.join(test_data_path, '22Na_point_source.yaml'), 'r') as na22:
@@ -56,6 +51,7 @@ class TestSpectroscopy(unittest.TestCase):
         # variables
         cls.energy_calibration = None
         cls.efficiency_calibration = None
+        cls.isotope_lib = isp.isotope_lib
                 
     @classmethod
     def tearDownClass(cls):
@@ -78,21 +74,21 @@ class TestSpectroscopy(unittest.TestCase):
     def test_energy_calibration(self):
         """Do energy calibration of detector channels to spectrum of 152-Eu source"""
         
-        energy_calib_bkg, _ = fit_background(x=self.Eu152_spectrum[0],
-                                             y=self.Eu152_spectrum[1])
+        energy_calib_bkg, _ = sp.fit_background(x=self.Eu152_spectrum[0],
+                                                y=self.Eu152_spectrum[1])
         
-        energy_calib_peaks = fit_spectrum(x=self.Eu152_spectrum[0],
-                                          y=self.Eu152_spectrum[1],
-                                          expected_peaks=self.energy_calib_peaks['channel'],
-                                          background=energy_calib_bkg)
+        energy_calib_peaks = sp.fit_spectrum(x=self.Eu152_spectrum[0],
+                                             y=self.Eu152_spectrum[1],
+                                             expected_peaks=self.energy_calib_peaks['channel'],
+                                             background=energy_calib_bkg)
         
         # check whether all peaks have been found
         self.assertListEqual(sorted(energy_calib_peaks.keys()), sorted(self.energy_calib_peaks['channel'].keys()))
         
-        self.energy_calibration= do_energy_calibration(observed_peaks=energy_calib_peaks,
-                                                       peak_energies=self.energy_calib_peaks['energy'])
+        self.energy_calibration= sp.do_energy_calibration(observed_peaks=energy_calib_peaks,
+                                                          peak_energies=self.energy_calib_peaks['energy'])
         
-        # 5 % error on fit parameters
+        # 50 % error on fit parameters
         self.assertTrue(all(self.energy_calibration['perr'] / self.energy_calibration['popt'] <= 0.5))
             
     def test_efficiency_calibration(self):
@@ -105,22 +101,22 @@ class TestSpectroscopy(unittest.TestCase):
         # generate expected peaks from source specs
         Eu152_expected = source_to_dict(self.Eu152_source_specs, info='lines')
         
-        efficiency_calib_bkg, _ = fit_background(x=self.Eu152_spectrum[0],
-                                                 y=self.Eu152_spectrum[1])
+        efficiency_calib_bkg, _ = sp.fit_background(x=self.Eu152_spectrum[0],
+                                                  y=self.Eu152_spectrum[1])
         
-        efficiency_calib_peaks = fit_spectrum(x=self.Eu152_spectrum[0],
-                                              y=self.Eu152_spectrum[1],
-                                              energy_cal=self.energy_calibration['func'],
-                                              expected_accuracy=self.energy_calibration['accuracy'],
-                                              expected_peaks=Eu152_expected,
-                                              background=efficiency_calib_bkg)
+        efficiency_calib_peaks = sp.fit_spectrum(x=self.Eu152_spectrum[0],
+                                                 y=self.Eu152_spectrum[1],
+                                                 energy_cal=self.energy_calibration['func'],
+                                                 expected_accuracy=self.energy_calibration['accuracy'],
+                                                 expected_peaks=Eu152_expected,
+                                                 background=efficiency_calib_bkg)
         
         # check whether all peaks have been found
         self.assertListEqual(sorted(efficiency_calib_peaks.keys()), sorted(Eu152_expected.keys()))
         
-        self.efficiency_calibration = do_efficiency_calibration(observed_peaks=efficiency_calib_peaks,
+        self.efficiency_calibration = sp.do_efficiency_calibration(observed_peaks=efficiency_calib_peaks,
                                                                 source_specs=self.Eu152_source_specs)
-        # 5 % error on fit parameters
+        # 50 % error on fit parameters
         self.assertTrue(all(self.efficiency_calibration['perr'] / self.efficiency_calibration['popt'] <= 0.5))
         
     def test_benchmark(self):
@@ -145,22 +141,20 @@ class TestSpectroscopy(unittest.TestCase):
         
         # test energy calibration by applying to Na22 and Ba133 spectra
         
-        # generate expected peaks and probabilities from source specs
+        # generate expected peaks from source specs
         Na22_expected = source_to_dict(self.Na22_source_specs, info='lines')
-        Na22_probs = source_to_dict(self.Na22_source_specs, info='probability')
-        
+
         # fit spectrum of source
-        Na22_peaks = fit_spectrum(x=self.Na22_spectrum[0],
-                                  y=self.Na22_spectrum[1],
-                                  energy_cal=self.energy_calibration['func'],
-                                  efficiency_cal=self.efficiency_calibration['func'],
-                                  t_spectrum=self.t_Na22,
-                                  expected_peaks=Na22_expected,
-                                  expected_accuracy=self.energy_calibration['accuracy'])
+        Na22_peaks = sp.fit_spectrum(x=self.Na22_spectrum[0],
+                                     y=self.Na22_spectrum[1],
+                                     energy_cal=self.energy_calibration['func'],
+                                     efficiency_cal=self.efficiency_calibration['func'],
+                                     t_spectrum=self.t_Na22,
+                                     expected_accuracy=self.energy_calibration['accuracy'])
         
-        # check whether all peaks have been found
-        self.assertListEqual(sorted(Na22_peaks.keys()), sorted(Na22_expected.keys()))
-        
+        # check whether all expected peaks have been found from library
+        self.assertTrue(all(ep in Na22_peaks for ep in Na22_expected.keys()))
+
         # check whether all activities are in Bq
         self.assertTrue(all('normalized' == Na22_peaks[p]['activity']['type'] for p in Na22_peaks))
         
@@ -169,36 +163,33 @@ class TestSpectroscopy(unittest.TestCase):
         
         # check whether all peaks are at correct energies within 1 per mille accuracy
         for na22_peak in Na22_peaks:
-            low, high = (x * Na22_expected[na22_peak] for x in (1 - self.energy_calibration['accuracy'], 1 + self.energy_calibration['accuracy']))
-            self.assertTrue(low <= Na22_peaks[na22_peak]['peak_fit']['popt'][0] <= high)
+            if na22_peak in Na22_expected:
+                low, high = (x * Na22_expected[na22_peak] for x in (1 - self.energy_calibration['accuracy'], 1 + self.energy_calibration['accuracy']))
+                self.assertTrue(low <= Na22_peaks[na22_peak]['peak_fit']['popt'][0] <= high)
             
-        # check for correct activity
-        Na22_activity_meas = calc_activity(Na22_peaks, Na22_probs)
-        Na22_activity_theo = get_activity(n0=np.array(self.Na22_source_specs['activity']),
-                                          half_life=self.Na22_source_specs['half_life'],
-                                          t_0=self.Na22_source_specs['timestamp_calibration'],
-                                          t_1=self.Na22_source_specs['timestamp_measurement'])
-        
+        # check for correct activity from library
+        Na22_activity_meas = sp.calc_activity(Na22_peaks)
+        Na22_activity_theo = sp.get_activity(n0=np.array(self.Na22_source_specs['activity']),
+                                             half_life=self.Na22_source_specs['half_life'],
+                                             t_0=self.Na22_source_specs['timestamp_calibration'],
+                                             t_1=self.Na22_source_specs['timestamp_measurement'])
+
         # check to see at least 90% of the expected activity; only order of magnitude relevant
         self.assertTrue(0.9 <= Na22_activity_meas['22_Na']['nominal'] / Na22_activity_theo[0] <= 1.0)
-        
+
         # generate expected peaks and probabilities from source specs
-        Ba133_expected = dict(('%i_%s_%i' % (self.Ba133_source_specs['A'],
-                                             self.Ba133_source_specs['symbol'], i) , l) for i, l in enumerate(self.Ba133_source_specs['lines']))
-        
-        Ba133_probs = dict(('%i_%s_%i' % (self.Ba133_source_specs['A'],
-                                          self.Ba133_source_specs['symbol'], i) , l) for i, l in enumerate(self.Ba133_source_specs['probability']))
+        Ba133_expected = source_to_dict(self.Ba133_source_specs, info='lines')
+
         # fit spectrum of source
-        Ba133_peaks = fit_spectrum(x=self.Ba133_spectrum[0],
-                                   y=self.Ba133_spectrum[1],
-                                   energy_cal=self.energy_calibration['func'],
-                                   efficiency_cal=self.efficiency_calibration['func'],
-                                   t_spectrum=self.t_Ba133,
-                                   expected_peaks=Ba133_expected,
-                                   expected_accuracy=self.energy_calibration['accuracy'])
+        Ba133_peaks = sp.fit_spectrum(x=self.Ba133_spectrum[0],
+                                      y=self.Ba133_spectrum[1],
+                                      energy_cal=self.energy_calibration['func'],
+                                      efficiency_cal=self.efficiency_calibration['func'],
+                                      t_spectrum=self.t_Ba133,
+                                      expected_accuracy=self.energy_calibration['accuracy'])
         
-        # check whether all peaks ghave been found
-        self.assertListEqual(sorted(Ba133_peaks.keys()), sorted(Ba133_expected.keys()))
+        # check whether all expected peaks have been found from library
+        self.assertTrue(all(ep in Ba133_peaks for ep in Ba133_expected.keys()))
         
         # check whether all activities are in Bq
         self.assertTrue(all('normalized' == Ba133_peaks[p]['activity']['type'] for p in Ba133_peaks))
@@ -208,15 +199,16 @@ class TestSpectroscopy(unittest.TestCase):
         
         # check whether all peaks are at correct energies within 1 per mill accuracy
         for ba133_peak in Ba133_peaks:
-            low, high = (x * Ba133_expected[ba133_peak] for x in (1 - self.energy_calibration['accuracy'], 1 + self.energy_calibration['accuracy']))
-            self.assertTrue(low <= Ba133_peaks[ba133_peak]['peak_fit']['popt'][0] <= high)
+            if ba133_peak in Ba133_expected:
+                low, high = (x * Ba133_expected[ba133_peak] for x in (1 - self.energy_calibration['accuracy'], 1 + self.energy_calibration['accuracy']))
+                self.assertTrue(low <= Ba133_peaks[ba133_peak]['peak_fit']['popt'][0] <= high)
             
         # check for correct activity
-        Ba133_activity_meas = calc_activity(Ba133_peaks, Ba133_probs)
-        Ba133_activity_theo = get_activity(n0=np.array(self.Ba133_source_specs['activity']),
-                                          half_life=self.Ba133_source_specs['half_life'],
-                                          t_0=self.Ba133_source_specs['timestamp_calibration'],
-                                          t_1=self.Ba133_source_specs['timestamp_measurement'])
+        Ba133_activity_meas = sp.calc_activity(Ba133_peaks)
+        Ba133_activity_theo = sp.get_activity(n0=np.array(self.Ba133_source_specs['activity']),
+                                              half_life=self.Ba133_source_specs['half_life'],
+                                              t_0=self.Ba133_source_specs['timestamp_calibration'],
+                                              t_1=self.Ba133_source_specs['timestamp_measurement'])
         
         # check to see at least 90% of the expected activity; only order of magnitude relevant
         self.assertTrue(0.9 <= Ba133_activity_meas['133_Ba']['nominal'] / Ba133_activity_theo[0] <= 1.0)
@@ -226,4 +218,3 @@ if __name__ == '__main__':
     logging.basicConfig(level=logging.INFO, format="%(asctime)s - %(name)s - [%(levelname)-8s] (%(threadName)-10s) %(message)s")
     suite = unittest.TestLoader().loadTestsFromTestCase(TestSpectroscopy)
     unittest.TextTestRunner(verbosity=2).run(suite)
-    
