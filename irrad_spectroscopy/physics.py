8af3c6b0559cc092baf3af425f68dbca2c009772
leloup314 <wolf@physik.uni-bonn.de>
2021-03-24 16:46:09 +0100

    ENH: add per isotope calculation of dose (rate), see #7

--- irrad_spectroscopy/physics.py
+++ irrad_spectroscopy/physics.py
@@ -2,7 +2,10 @@
 
 import logging
 import numpy as np
+from scipy.integrate import quad
 from irrad_spectroscopy import xray_coefficient_table, xray_coefficient_table_file
+from irrad_spectroscopy.spec_utils import get_isotope_info
+
 
 
 def decay_constant(half_life):
@@ -30,7 +33,7 @@ def gamma_dose_rate(energy, probability, activity, distance, material='air'):
     energy : float
         gamma energy
     probability : float from 0 to 1
-        probability of emitting this gamm per disintegration
+        probability of emitting this gamma per disintegration
     activity : float
         disintegrations per second (Bq)
     distance : float
@@ -69,3 +72,71 @@ def gamma_dose_rate(energy, probability, activity, distance, material='air'):
         tmp_xray_en_ab_interp = np.interp(energy, xray_energies[idx:idx+2], xray_en_absorption[idx:idx+2])
 
     return custom_factor * energy * probability * tmp_xray_en_ab_interp * activity / distance**2.
+
+
+def isotope_dose_rate(isotope, activity, distance, material='air', time=None):
+    """
+    Calculation of the per-isotope dose rate in *material* according to hps.org/publicinformation/ate/faqs/gammaandexposure.html
+
+    Parameters
+    ----------
+    isotope : str
+        identifier according to
+    probability : float from 0 to 1
+        probability of emitting this gamma per disintegration
+    activity : float
+        disintegrations per second (Bq)
+    distance : float
+        distance in cm from gamma source the dose rate should be calculated at
+    material : str
+        string of material the dose is to be calculated in. Must be key in xray_coeffs
+    time : int, float
+        time to integrate over in hours
+
+    Returns
+    -------
+
+    dose_rate: float
+        dose rate from gamma in uSv/h
+    """
+
+    if material not in xray_coefficient_table['material'].keys():
+        msg = 'No x-Ray coefficient table for material "{}". Please add table to {}.'.format(material, xray_coefficient_table_file)
+        raise KeyError(msg)
+
+    if not isinstance(isotope, (list, tuple)):
+        isotope = [isotope]
+        if any(not isinstance(iso, str) for iso in isotope):
+            raise TypeError('*isotope* must be str or list of strings with identifiers.')
+
+    if not isinstance(activity, (list, tuple)):
+        activity = [activity]
+        if any(not isinstance(act, (float, int)) for act in activity) or len(activity) != len(isotope):
+            raise TypeError('*activity* must be number or list of number with corresponding to *isotope*.')
+
+
+    half_lifes = None if time is None else get_isotope_info(info='half_life')
+
+    total_dose_rate = {}
+
+    for iso, act in zip(isotope, activity):
+
+        # Get intensity and energy of isotope lines
+        iso_probabilities = get_isotope_info(info='probability', iso_filter=iso)
+        iso_energies = get_isotope_info(info='lines', iso_filter=iso)
+
+        total_dose_rate[iso] = 0  # uSv/h
+
+        for line in iso_energies:
+
+            total_dose_rate[iso] += gamma_dose_rate(energy=iso_energies[line],
+                                                    probability=iso_probabilities[line],
+                                                    activity=act,
+                                                    distance=distance,
+                                                    material=material)
+
+        # Integrate over time returning absolut dose in uSv
+        if half_lifes:
+            total_dose_rate[iso], _ = quad(decay_law, 0, time, args=(total_dose_rate[iso], half_lifes[iso]/60.**2))
+
+    return total_dose_rate
