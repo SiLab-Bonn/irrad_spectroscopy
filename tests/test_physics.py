f01e67008860f96366f122a399ac2505fb78edea
leloup314 <wolf@physik.uni-bonn.de>
2022-08-11 16:45:24 +0200

    ENH: add tests for isotope dose rate calulations cross checked via http://www.radprocalculator.com/

---
+++ tests/test_physics.py
@@ -0,0 +1,66 @@
+
+import unittest
+import logging
+import math
+import irrad_spectroscopy.physics as physics
+
+
+class TestPhysics(unittest.TestCase):
+    
+    @classmethod
+    def setUpClass(cls):
+
+        # Reference results from http://www.radprocalculator.com/Gamma.aspx @ 09/08/2022
+        cls.isotope_dose_rates = {'65_Zn': {'kwargs': {'distance': 10,  # cm
+                                                       'material': 'air',
+                                                       'activity': 1e6},  # Bq
+                                            'result': 7.32721329726743},  # uSv/h
+                                  
+                                  '58_Co': {'kwargs': {'distance': 10,  # cm
+                                                       'material': 'air',
+                                                       'activity': 1e6},  # Bq
+                                            'result': 12.7792816179285},  # uSv/h
+                                  
+                                  '133_Ba': {'kwargs': {'distance': 10,  # cm
+                                                        'material': 'air',
+                                                        'activity': 1e6},  # Bq
+                                             'result': 4.73961723205635},
+                                  
+                                  '241_Am': {'kwargs': {'distance': 10,  # cm
+                                                        'material': 'air',
+                                                        'activity': 1e6},  # Bq
+                                             'result': 0.39675657840774},
+
+                                  '177_Lu': {'kwargs': {'distance': 10,  # cm
+                                                        'material': 'air',
+                                                        'activity': 1e6},  # Bq
+                                             'result': 0.363019036969211},
+
+                                  '40_K': {'kwargs': {'distance': 10,  # cm
+                                                       'material': 'air',
+                                                       'activity': 1e6},  # Bq
+                                            'result': 1.84849188821643},
+                                }
+                
+    @classmethod
+    def tearDownClass(cls):
+        pass
+
+    def test_gamma_dose_rate(self):
+        pass
+
+    def test_isotope_dose_rate(self):
+        
+        for isotope, data in self.isotope_dose_rates.items():
+            test_result = physics.isotope_dose_rate(isotope=isotope, **data['kwargs'])[isotope]
+            print(test_result, data['result'], isotope)
+            assert math.isclose(test_result, data['result'], rel_tol=0.25)
+
+    def test_fluence_from_activity(self):
+        pass
+
+
+if __name__ == '__main__':
+    logging.basicConfig(level=logging.INFO, format="%(asctime)s - %(name)s - [%(levelname)-8s] (%(threadName)-10s) %(message)s")
+    suite = unittest.TestLoader().loadTestsFromTestCase(TestPhysics)
+    unittest.TextTestRunner(verbosity=2).run(suite)
